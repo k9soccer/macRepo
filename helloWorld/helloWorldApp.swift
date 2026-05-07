cbcb98a175c1aee1cee1a1ec00fffdf8ed81906d
drewDabe <ddabe@email.sc.edu>
2026-05-07 13:00:23 -0400

    Initial Commit

---
+++ helloWorld/helloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldApp.swift
+//  helloWorld
+//
+//  Created by Drew Dabe on 2026/5/7.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
